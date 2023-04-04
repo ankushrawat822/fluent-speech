f37780c6a7cdd3a6052ca9bd12afdd1dd89db34a
ankushrawat822 <ankushrawat037@gmail.com>
2023-04-04 20:50:33 +0530

    navbar mobile

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,47 +1,152 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { Link } from "react-router-dom";
 import logo from '../../images/nav/logo-1.png'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {VscGlobe} from 'react-icons/vsc'
-import {MdOutlineKeyboardArrowDown} from 'react-icons/md'
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { VscGlobe } from 'react-icons/vsc'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import {RxCross2} from 'react-icons/rx'
 
 const Navbar = () => {
+
+    // states for drop down menus
+   const [showHomeMenu , setShowHomeMenu] = useState(false)
+   const [showSolutionMenu , setShowSolutionMenu] = useState(false)
+
+
+   // states for hamburder menu
+   const [menuIcon , setMenuIcon] = useState(false)
+
+   //    state to show mabile nav links
+ const [ShowMobileNavDiv, setShowMobileNavDiv] = useState(false)
+
+//  state for showing home menu links
+const [showHomeMenuLinks , setShowHomeMenuLinks] = useState(false)
+
+//  state for showing home menu links
+const [showSolutionMenuLinks , setShowSolutionMenuLinks] = useState(false)
+
+   const handleCrossIcon = ()=>{
+      setMenuIcon(false)
+      setShowMobileNavDiv(false)
+   }
+
+
+
+   const handleHamburgerIcon = ()=>{
+    setMenuIcon(true)
+    setShowMobileNavDiv(true)
+ }
     return (
         <>
 
-      
-        {/* // main div starts */}
-        <div className=' flex justify-between px-4 items-center lg:justify-start py-3 lg:py-6 gap-20'>
-            {/* logo div */}
-            <div>
-                <img className='md:ml-4 w-[150px] h-[45px]' src={logo} alt="logo" />
+
+            {/* // main div starts */}
+            <div className='relative flex justify-between px-4 items-center lg:justify-start  gap-x-20'>
+                {/* logo div */}
+                <div>
+                  <Link to='/'> <img className='md:ml-4 w-[150px] h-[45px] my-3 lg:my-0' src={logo} alt="logo" /></Link> 
+                </div>
+                {/* links div */}
+                <div className='hidden lg:flex '>
+                    <ul className='flex items-center justify-center text-[18px] gap-x-14'>
+                        <li onMouseEnter={()=> setShowHomeMenu(true)} onMouseLeave={()=> setShowHomeMenu(false)} className='flex items-center justify-center h-[93px]'><Link className='flex items-center justify-center' to="/"> Home <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown></Link>
+                         {/* home drop down menues start */}
+                       { showHomeMenu && <div onMouseEnter={()=> setShowHomeMenu(true)} className=' absolute top-[90px] bg-white '>
+                            <ul className=' flex flex-col items-center justify-center gap-2 p-4 emerging-tech-white-div-drop-shadow text-[18px]'>
+                                <li className='cursor-pointer px-2 py-1 rounded-[6px] hover:w-full hover:text-center hover:bg-[#E8E8E8]'>Driver</li>
+                                <li className='cursor-pointer px-2 py-1 rounded-[6px] hover:w-full hover:text-center hover:bg-[#E8E8E8]'>Talk</li>
+                                <li className='px-2 py-1 rounded-[6px] hover:w-full hover:text-center cursor-pointer hover:bg-[#E8E8E8]'>About Us</li>
+                                <li className='cursor-pointer px-2 py-1 rounded-[6px] hover:w-full hover:text-center hover:bg-[#E8E8E8]'>OUr Partial Client</li>
+                            </ul>
+                        </div> }
+                          {/* home drop down menues ends */}
+                          </li>
+
+
+                       
+
+
+                        <li onMouseEnter={()=> setShowSolutionMenu(true)} onMouseLeave={()=> setShowSolutionMenu(false)} className='flex items-center justify-center  h-[93px]'>Solutions  <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown> {/* home drop down menues start */}
+                       { showSolutionMenu && <div onMouseEnter={()=> setShowSolutionMenu(true)} className=' absolute top-[90px] bg-white '>
+                            <ul className=' flex flex-col items-center justify-center gap-2 p-4 emerging-tech-white-div-drop-shadow text-[18px]'>
+                                <li className='cursor-pointer px-2 py-1 rounded-[6px] hover:w-full hover:text-center hover:bg-[#E8E8E8]'><Link to='/emerging-tehcnologies'>Emerging Technologies</Link></li>
+                               
+                            </ul>
+                        </div> }
+                          {/* home drop down menues ends */}</li>
+                        <li><Link to="/partner"> Partner Model</Link></li>
+                        <li><Link to="/career">Careers</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                    </ul>
+                </div>
+                {/* search and language div */}
+                <div className='hidden xl:flex ml-10 items-center justify-center gap-1'>
+                    <AiOutlineSearch className='text-[20px] '></AiOutlineSearch>
+                    <p>English/Other language</p>
+                    <VscGlobe className='text-[23px] font-bold'></VscGlobe>
+                </div>
+
+                <VscGlobe className='text-[23px] font-bold hidden lg:flex xl:hidden'></VscGlobe>
+
+
+                {/* mobile responsive  */}
+                <div className='lg:hidden'>
+                   {menuIcon ? ( <RxCross2 onClick={handleCrossIcon} className='text-[27px] font-bold'></RxCross2>) : <GiHamburgerMenu onClick={handleHamburgerIcon} className='text-[27px] font-bold'></GiHamburgerMenu> } 
+                </div>
             </div>
-            {/* links div */}
-            <div className='hidden lg:flex'>
-                <ul className='flex items-center justify-center text-[18px] gap-14'>
-                    <li className='flex items-center justify-center'><Link className='flex items-center justify-center' to="/"> Home <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown></Link></li>
-                    <li  className='flex items-center justify-center'><Link className='flex items-center justify-center' ><Link to='/emerging-tehcnologies'>Solutions</Link>  <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown></Link></li>
-                    <li><Link to="/partner"> Partner Model</Link></li>
-                    <li><Link to="/career">Careers</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
+
+
+
+           {/* mobile links   */}
+         { 
+            ShowMobileNavDiv && 
+            <div className='absolute top-[68px]  w-full bg-white '>
+                <ul>
+                    <li onClick={(prev)=>setShowHomeMenuLinks((prev)=> !prev)} className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'>Home <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown></li>
+                    {
+                        showHomeMenuLinks && 
+                        <div>
+                            <ul className='text-[19px] sm:text-[20px]'>
+                            <li className='cursor-pointer px-2 py-1 rounded-[6px] w-full text-center hover:bg-[#E8E8E8]'>Driver</li>
+                                <li className='cursor-pointer px-2 py-1 rounded-[6px] w-full text-center hover:bg-[#E8E8E8]'>Talk</li>
+                                <li className='px-2 py-1 rounded-[6px] w-full text-center cursor-pointer hover:bg-[#E8E8E8]'>About Us</li>
+                            </ul>
+                        </div>
+                    }
                 </ul>
-            </div>
-            {/* search and language div */}
-            <div className='hidden xl:flex ml-10 items-center justify-center gap-1'>
-            <AiOutlineSearch className='text-[20px] '></AiOutlineSearch>
-                <p>English/Other language</p>
-                <VscGlobe className='text-[23px] font-bold'></VscGlobe>
-            </div>
+                <ul>
+                    <li onClick={(prev)=>setShowSolutionMenuLinks((prev)=> !prev)} className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'>Solutions <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown></li>
+                    {
+                        showSolutionMenuLinks && 
+                        <div>
+                            <ul className='text-[19px] sm:text-[20px]'>
+                            <li className='cursor-pointer px-2 py-1 rounded-[6px] w-full text-center hover:bg-[#E8E8E8]'><Link to='/emerging-tehcnologies'>Emerging Technologies</Link></li>
+                              
+                            </ul>
+                        </div>
+                    }
+                </ul>
 
-            <VscGlobe className='text-[23px] font-bold hidden lg:flex xl:hidden'></VscGlobe>
+                <ul>
+                    <li className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'>Partner Model </li> 
+                    <li className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'> Careers </li>
+                    <li className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'> <Link to='/contact'>Contact Us</Link> </li>
+                    <li className='flex items-center justify-center text-[22px] sm:text-[24px] p-2'>
+                     {/* search and language div */}
+                <div className='flex  items-center justify-center gap-1'>
+                    <AiOutlineSearch className='text-[20px] '></AiOutlineSearch>
+                    <p className='text-[18px]'>English/Other language</p>
+                    <VscGlobe className='text-[24px] font-bold mt-2'></VscGlobe>
+                </div>
 
+                
+ </li>  
 
-            {/* mobile responsive  */}
-            <div className='lg:hidden'>
-        <GiHamburgerMenu className='text-[27px] font-bold'></GiHamburgerMenu>
+                </ul>
+                
             </div>
-        </div>
+         }  
 
 
         </>
--- src/components/pages/solution/EmergingTechnologies.jsx
+++ src/components/pages/solution/EmergingTechnologies.jsx
@@ -15,7 +15,7 @@ const EmergingTechnologies = () => {
             </div>
 
             {/* we provide services that include.... */}
-            <div className='px-3 sm:px-20 md:px-44 '>
+            <div className='px-3 sm:px-20 md:px-44 mb-10'>
                 <p className='text-[28px] font-semibold leading-[38px] sm:leading-[58px] lg:my-9 text-center mt-5'>WE WANT TO HEAR FROM YOU</p>
             </div>
 
@@ -27,11 +27,11 @@ const EmergingTechnologies = () => {
                     <div className='absolute px-3 left-0 right-0 ml-auto mr-auto w-full flex flex-col items-center justify-center top-4  text-white '>
                         <div className='emerg-tech-card-1-white-tick-img'></div>
                         <div>
-                        <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
-                        <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
-                        <p>Read more...</p>
+                            <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
+                            <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
+                            <p>Read more...</p>
                         </div>
-                       
+
                     </div>
                     {/* front div text  ends*/}
                     <div className='emerg-tech-card-1-back-img flex items-center justify-center'>
@@ -46,11 +46,11 @@ const EmergingTechnologies = () => {
                     <div className='absolute px-3 left-0 right-0 ml-auto mr-auto w-full flex flex-col items-center justify-center top-4  text-white '>
                         <div className='emerg-tech-card-1-white-tick-img'></div>
                         <div>
-                        <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
-                        <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
-                        <p>Read more...</p>
+                            <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
+                            <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
+                            <p>Read more...</p>
                         </div>
-                       
+
                     </div>
                     {/* front div text  ends*/}
                     <div className='emerg-tech-card-1-back-img flex items-center justify-center'>
@@ -64,11 +64,11 @@ const EmergingTechnologies = () => {
                     <div className='absolute px-3 left-0 right-0 ml-auto mr-auto w-full flex flex-col items-center justify-center top-4  text-white '>
                         <div className='emerg-tech-card-1-white-tick-img'></div>
                         <div>
-                        <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
-                        <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
-                        <p>Read more...</p>
+                            <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
+                            <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
+                            <p>Read more...</p>
                         </div>
-                       
+
                     </div>
                     {/* front div text  ends*/}
                     <div className='emerg-tech-card-1-back-img flex items-center justify-center'>
@@ -83,11 +83,11 @@ const EmergingTechnologies = () => {
                     <div className='absolute px-3 left-0 right-0 ml-auto mr-auto w-full flex flex-col items-center justify-center top-4  text-white '>
                         <div className='emerg-tech-card-1-white-tick-img'></div>
                         <div>
-                        <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
-                        <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
-                        <p>Read more...</p>
+                            <p className='  font-semibold  text-[24px] text-start my-3'>Proof of Concept</p>
+                            <p className='text-start text-white text-[16px] mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at sem id purus viverra porttitor.</p>
+                            <p>Read more...</p>
                         </div>
-                       
+
                     </div>
                     {/* front div text  ends*/}
                     <div className='emerg-tech-card-1-back-img flex items-center justify-center'>
@@ -97,29 +97,29 @@ const EmergingTechnologies = () => {
             </div>
 
             {/* blue bg  div  */}
-            <div className='emerg-tech-blue-div px-3 py-8 my-10 md:px-20 lg:px-28 xl:px-36 mb-20'>
-            <p className='text-[20px] sm:text-[24px] text-white text-center '>OUR EMERGING TECHNOLOGIES PRACTICE HELPS CUSTOMERS REALIZE THE DREAM THAT WILL SHAPE TOMORROW’S WORLD TO STAY COMPETITIVE AND AHEAD OF THE CURVE.</p>
+            <div className='emerg-tech-blue-div px-3 py-14 my-10 md:px-20 lg:px-28 xl:px-36 mb-20'>
+                <p className='text-[20px] sm:text-[24px] text-white text-center '>OUR EMERGING TECHNOLOGIES PRACTICE HELPS CUSTOMERS REALIZE THE DREAM THAT WILL SHAPE TOMORROW’S WORLD TO STAY COMPETITIVE AND AHEAD OF THE CURVE.</p>
             </div>
             {/* 3 wide text boxes */}
             <div className='flex flex-col items-center justify-center gap-9'>
                 {/* box 1 */}
-                <div className='emerging-tech-white-div-drop-shadow flex flex-col items-start justify-start gap-2 text-start p-5 w-full sm:w-[80%] lg:w-[70%] rounded-[10px]'>
+                <div className='emerging-tech-white-div-drop-shadow flex flex-col items-start justify-start gap-2 text-start p-5  sm:w-[80%] lg:w-[70%] rounded-[10px] mx-3 '>
                     <p className='text-[22px] sm:text-[24px] font-semibold'>The Internet of Things (IoT):</p>
                     <p className='text-[20px] sm:text-[20px] leading-[30px] text-start'>IoT impacts everything from the way smart homes function to Manufacturing 4.0. Agile Global is in a unique position to help you implement IoT solutions from Proof of Concept (POC) to full scale implementation.</p>
                 </div>
 
-                 {/* box 2 */}
-                 <div className='emerging-tech-white-div-drop-shadow bg-[#023267] flex flex-col items-start justify-star gap-2 text-start p-5 w-full sm:w-[80%] lg:w-[70%] rounded-[10px] text-white'>
+                {/* box 2 */}
+                <div className='emerging-tech-white-div-drop-shadow bg-[#023267] flex flex-col items-start justify-star gap-2 text-start p-5 mx-3 sm:w-[80%] lg:w-[70%] rounded-[10px] text-white'>
                     <p className='text-[22px] sm:text-[24px] font-semibold'>Artificial Intelligence/Machine Learning (AI/ML):</p>
                     <p className='text-[20px] sm:text-[20px] leading-[30px] text-start'>
-Unlock the value of unstructured to structured data. Agile Global can help analyze and implement AI/ML functionality from Integrated Analytics, Performance Dashboards to complex AI/ML applications. </p>
+                        Unlock the value of unstructured to structured data. Agile Global can help analyze and implement AI/ML functionality from Integrated Analytics, Performance Dashboards to complex AI/ML applications. </p>
                 </div>
 
-                 {/* box 3 */}
-                 <div className='emerging-tech-white-div-drop-shadow flex flex-col items-start justify-star gap-2 text-start p-5 w-full sm:w-[80%] lg:w-[70%] rounded-[10px]'>
+                {/* box 3 */}
+                <div className='emerging-tech-white-div-drop-shadow flex flex-col items-start justify-star gap-2 text-start p-5 mx-3 sm:w-[80%] lg:w-[70%] rounded-[10px]'>
                     <p className='text-[22px] sm:text-[24px] font-semibold'>Robotics Process Automation (RPA):</p>
                     <p className='text-[20px] sm:text-[20px] leading-[30px] text-start'>
-More CIOs are turning to robotic process automation to eliminate tedious tasks, freeing corporate workers to focus on higher value work. But RPA requires proper design, planning and governance if it's to bolster the business, experts say.</p>
+                        More CIOs are turning to robotic process automation to eliminate tedious tasks, freeing corporate workers to focus on higher value work. But RPA requires proper design, planning and governance if it's to bolster the business, experts say.</p>
                 </div>
             </div>
 
@@ -129,7 +129,7 @@ More CIOs are turning to robotic process automation to eliminate tedious tasks, 
                 <p className='text-[28px] sm:text-[34px] md:text-[48px] font-bold text-center my-5'>Our <span className='text-[#17519B]'>Recognitions</span> & Awards</p>
 
                 {/* cards div */}
-                <div className='flex flex-col sm:flex-row flex-wrap items-center justify-center gap-4'>
+                <div className='flex flex-col sm:flex-row flex-wrap items-center justify-center gap-14 lg:gap-4'>
                     {/* card 1 */}
                     <div className=' sm:w-[387px] flex flex-col items-start justify-start gap-4'>
                         <img src={LastCardImg1} alt="" />
@@ -137,15 +137,15 @@ More CIOs are turning to robotic process automation to eliminate tedious tasks, 
                         <p>Lorem ipsum dolor sit amet consectetur. Tortor amet placerat sed malesuada quisque mauris est. Egestas non dictum purus.</p>
                     </div>
 
-                     {/* card 2*/}
-                     <div className='sm:w-[387px] flex flex-col items-start justify-start gap-4'>
+                    {/* card 2*/}
+                    <div className='sm:w-[387px] flex flex-col items-start justify-start gap-4'>
                         <img src={LastCardImg2} alt="" />
                         <p className='font-semibold text-[24px]'>Beautiful View </p>
                         <p>Lorem ipsum dolor sit amet consectetur. Tortor amet placerat sed malesuada quisque mauris est. Egestas non dictum purus.</p>
                     </div>
 
-                     {/* card 3 */}
-                     <div className='sm:w-[387px] flex flex-col items-start justify-start gap-4'>
+                    {/* card 3 */}
+                    <div className='sm:w-[387px] flex flex-col items-start justify-start gap-4'>
                         <img src={LastCardImg3} alt="" />
                         <p className='font-semibold text-[24px]'>Fixing Rocket with Your Hand</p>
                         <p>Lorem ipsum dolor sit amet consectetur. Tortor amet placerat sed malesuada quisque mauris est. Egestas non dictum purus.</p>
